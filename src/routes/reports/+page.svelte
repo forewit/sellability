3854858aa4da0121f24aee14ac2474f118bf9291
Marc Anderson <marcda495@gmail.com>
2025-04-23 12:43:14 -0400

    'na'

--- src/routes/reports/+page.svelte
+++ src/routes/reports/+page.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-  import * as Card from "$lib/components/ui/card/index.js";
   import Goals from "$lib/components/app/goals.svelte";
   import Report from "$lib/components/app/report.svelte";
 </script>
 
-    <div class="grid md:grid-cols-[1fr,3fr] gap-8 p-6">
-      <Goals class="max-w-[250px]"/>
-      <Report />
-    </div>
+<div class="grid md:grid-cols-[1fr,3fr] gap-8 p-6">
+  <Goals class="max-w-[250px]" />
+  <Report />
+</div>
