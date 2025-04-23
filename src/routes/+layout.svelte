8b0ae38d996429128bd47bb4fa823a3e988b1ecf
Marc Anderson <marcda495@gmail.com>
2025-04-23 17:10:09 +0000

    na

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import "../app.css";
+  import { cn } from "$lib/utils";
   import { setAppContext } from "$lib/components/app/app.svelte";
   import Toolbar from "$lib/components/app/toolbar.svelte";
   import * as Sheet from "$lib/components/ui/sheet/index.js";
   import Product from "$lib/components/app/product.svelte";
   import { ScrollArea } from "$lib/components/ui/scroll-area/index.js";
   import SafeAreas from "$lib/components/safeareas/safe-areas.svelte";
+  import { IsMobile } from "$lib/hooks/is-mobile.svelte";
 
   let { children } = $props();
 
@@ -15,6 +17,8 @@
   function onOpenChange(open: boolean) {
     if (!open) app.selectedProductId = "";
   }
+
+  const isMobile = new IsMobile();
 </script>
 
 <svelte:head>
@@ -24,18 +28,13 @@
 <SafeAreas>
   <!-- main page with toolbar -->
   <div
-    class="h-screen bg-red-50 grid grid-rows-[1fr,auto] md:grid-cols-[auto,1fr] pl-[var(--safe-area-left)] pr-[var(--safe-area-right)] pt-[var(--safe-area-top)]"
+    class="h-svh w-svw grid relative bg-red-50 pl-[var(--safe-area-left)] pr-[var(--safe-area-right)] pt-[var(--safe-area-top)]"
   >
-    <Toolbar
-      class="place-self-center flex-row md:flex-col w-min h-min order-2 md:order-1 m-2 mt-0 md:mt-2 md:mr-0"
-    />
-    <ScrollArea
-      class="mb-2 border shadow-sm rounded-lg bg-card order-1 md:order-2"
-      style="margin-left: max(0.5rem, var(--safe-area-left)); margin-right: max(0.5rem, var(--safe-area-right)); margin-top: max(0.5rem, var(--safe-area-top));"
-      type="scroll"
-    >
+    <ScrollArea type="scroll">
       {@render children?.()}
+      <div class="h-16"></div>
     </ScrollArea>
+    <Toolbar class="flex-row absolute left-1/2 -translate-x-1/2 bottom-4" />
   </div>
 
   <!-- product editing sheet -->
