33aec4c16d086c41ba7d3bc3f4da89d01248d850
Marc Anderson <marcda495@gmail.com>
2025-04-23 12:37:23 -0400

    'na'

---
+++ src/lib/components/safeareas/safe-areas.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import "./safeareas.css";
+  import { onMount, type Snippet } from "svelte";
+
+  let { children }: { children: Snippet } = $props();
+
+  function updateSafeAreas() {
+    if (!screen) throw new Error("Cannot update safe areas: screen is not defined");
+
+    const root = document.documentElement;
+    switch (screen.orientation.type) {
+      case "portrait-primary":
+        root.style.setProperty("--safe-area-top", "env(safe-area-inset-top)");
+        root.style.setProperty("--safe-area-left", "0px");
+        root.style.setProperty("--safe-area-right", "0px");
+        break;
+      case "landscape-primary":
+        root.style.setProperty("--safe-area-top", "0px");
+        root.style.setProperty("--safe-area-left", "env(safe-area-inset-left)");
+        root.style.setProperty("--safe-area-right", "0px");
+        break;
+      case "landscape-secondary":
+        root.style.setProperty("--safe-area-top", "0px");
+        root.style.setProperty("--safe-area-left", "0px");
+        root.style.setProperty("--safe-area-right", "env(safe-area-inset-right)");
+        break;
+    }
+  }
+
+  onMount(() => {
+    screen.orientation.addEventListener("change", updateSafeAreas);
+    return () => {
+      screen.orientation.removeEventListener("change", updateSafeAreas);
+    };
+  });
+</script>
+
+{@render children()}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
   import * as Sheet from "$lib/components/ui/sheet/index.js";
   import Product from "$lib/components/app/product.svelte";
   import { ScrollArea } from "$lib/components/ui/scroll-area/index.js";
+  import SafeAreas from "$lib/components/safeareas/safe-areas.svelte";
 
   let { children } = $props();
 
@@ -20,32 +21,32 @@
   <title>Sellability</title>
 </svelte:head>
 
-<!-- main page with toolbar -->
-<div
-  class="h-screen bg-red-50 grid grid-rows-[1fr,auto] md:grid-cols-[auto,1fr]"
-  style="padding-left: max(0px, env(safe-area-inset-left)); padding-right: max(0px, env(safe-area-inset-right)); padding-top: max(0px, env(safe-area-inset-top));"
->
-  <Toolbar
-    class="place-self-center flex-row md:flex-col w-min h-min order-2 md:order-1 m-2 mt-0 md:mt-2 md:mr-0"
-  />
-  <ScrollArea
-    class="mb-2 border shadow-sm rounded-lg bg-card order-1 md:order-2"
-    style="margin-left: min(0.5rem, env(safe-area-inset-left)); margin-right: min(0.5rem, env(safe-area-inset-right)); margin-top: min(0.5rem, env(safe-area-inset-top));"
-    type="scroll"
+<SafeAreas>
+  <!-- main page with toolbar -->
+  <div
+    class="h-screen bg-red-50 grid grid-rows-[1fr,auto] md:grid-cols-[auto,1fr] pl-[var(--safe-area-left)] pr-[var(--safe-area-right)] pt-[var(--safe-area-top)]"
   >
-    {@render children?.()}
-  </ScrollArea>
-</div>
-
-<!-- product editing sheet -->
-<Sheet.Root open={productSheetOpen} {onOpenChange}>
-  <Sheet.Content
-    class="p-0"
-    style="padding-right: max(0px, env(safe-area-inset-right)); padding-top: max(0px, env(safe-area-inset-top));"
-    hideCloseButton
-  >
-    <ScrollArea class="h-full" type="scroll">
-      <Product product={app.selectedProduct} class="p-6" />
+    <Toolbar
+      class="place-self-center flex-row md:flex-col w-min h-min order-2 md:order-1 m-2 mt-0 md:mt-2 md:mr-0"
+    />
+    <ScrollArea
+      class="mb-2 border shadow-sm rounded-lg bg-card order-1 md:order-2"
+      style="margin-left: max(0.5rem, var(--safe-area-left)); margin-right: max(0.5rem, var(--safe-area-right)); margin-top: max(0.5rem, var(--safe-area-top));"
+      type="scroll"
+    >
+      {@render children?.()}
     </ScrollArea>
-  </Sheet.Content>
-</Sheet.Root>
+  </div>
+
+  <!-- product editing sheet -->
+  <Sheet.Root open={productSheetOpen} {onOpenChange}>
+    <Sheet.Content
+      class="p-0 pr-[var(--safe-area-right)] pt-[var(--safe-area-top)]"
+      hideCloseButton
+    >
+      <ScrollArea class="h-full" type="scroll">
+        <Product product={app.selectedProduct} class="p-6" />
+      </ScrollArea>
+    </Sheet.Content>
+  </Sheet.Root>
+</SafeAreas>
